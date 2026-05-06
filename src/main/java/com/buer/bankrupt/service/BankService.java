6b59d1428ae74a732ed16479f9b78ac1e5483d0a
Arka Aceananda <aceanandptr12@gmail.com>
2026-05-06 14:09:36 +0700

    Create service and console UI

--- src/main/java/com/buer/bankrupt/service/BankService.java
+++ src/main/java/com/buer/bankrupt/service/BankService.java
@@ -1,4 +1,64 @@
 package com.buer.bankrupt.service;
 
+import com.buer.bankrupt.model.User;
+import com.buer.bankrupt.repository.UserRepository;
+import java.util.List;
+
 public class BankService {
+    private final UserRepository userRepository;
+    private User currentUser;
+
+    public BankService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean logic(String account, String pin) {
+        List<User> users = userRepository.findAll();
+        for (User u : users) {
+            if (u.getAccount().equals(account) && u.getPin().equals(pin)) {
+                this.currentUser = u;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean transfer(String norekDest, double amount) {
+        List<User> users = userRepository.findAll();
+        if (currentUser == null || amount > currentUser.getSaldo() || amount <= 0) {
+            return false;
+        }
+
+        User targetUser = null;
+        for (User u : users) {
+            if (u.getAccount().equals(norekDest)) {
+                targetUser = u;
+                break;
+            }
+        }
+
+        if (targetUser != null && !targetUser.getAccount().equals(currentUser.getAccount())) {
+            for (User u : users) {
+                if (u.getAccount().equals(currentUser.getAccount())) {
+                    u.setSaldo(u.getSaldo() - amount);
+                }
+                if (u.getAccount().equals(targetUser.getAccount())) {
+                    u.setSaldo(u.getSaldo() + amount);
+                }
+            }
+
+            userRepository.saveAll(users);
+            currentUser.setSaldo(currentUser.getSaldo() - amount);
+            return true;
+        }
+        return false;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void logout() {
+        this.currentUser = null;
+    }
 }
--- src/main/java/com/buer/bankrupt/ui/MenuHandler.java
+++ src/main/java/com/buer/bankrupt/ui/MenuHandler.java
@@ -1,4 +1,48 @@
 package com.buer.bankrupt.ui;
 
+import com.buer.bankrupt.repository.UserRepository;
+import com.buer.bankrupt.service.BankService;
+import java.util.Scanner;
+
 public class MenuHandler {
+    UserRepository repository = new UserRepository();
+    BankService service = new BankService(repository);
+    static void main() {
+        showMainMenu();
+    }
+
+    static void showMainMenu() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            label:
+            while (true) {
+                System.out.println("Welcome to Bankrupt Menu");
+                System.out.println("""
+                        1. Login
+                        2. Create New Account
+                        3. Exit""".stripIndent());
+
+                try {
+                    String input;
+                    input = scanner.nextLine();
+                    if (input.isBlank()) {
+                        System.err.println("Please choose an option.");
+                    }
+                    switch (input) {
+                        case "1":
+                            System.out.println("Enter your username: ");
+                            String name = scanner.nextLine();
+                            if (name == null || name.isBlank()) {
+                                System.err.println("[!!] Name cannot be null.");
+                                break;
+                            }
+                        case "2": {
+                            // TODO
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
